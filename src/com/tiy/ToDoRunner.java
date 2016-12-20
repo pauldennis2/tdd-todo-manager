61ef3c2786091ae70ecfcffd9bd17830213370bc
Paul Dennis <pauldennis.misc@gmail.com>
2016-12-19 20:24:18 -0500

    Good commit. Todo list works. No bells/whistles.

--- src/com/tiy/ToDoRunner.java
+++ src/com/tiy/ToDoRunner.java
@@ -3,9 +3,14 @@ package com.tiy;
 import jodd.json.JsonParser;
 import jodd.json.JsonSerializer;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.jar.Pack200;
 
 /**
  * Created by erronius on 12/19/2016.
@@ -13,7 +18,10 @@ import java.util.Scanner;
 public class ToDoRunner {
 
     Scanner scanner;
-    List<ToDoItem> todoList;
+    //List<ToDoItem> todoList;
+    ToDoList todoList;
+
+    public final static String FILE_NAME = "todo.json";
 
     public static void main(String[] args) {
         new ToDoRunner().startInterface();
@@ -21,45 +29,85 @@ public class ToDoRunner {
 
     public ToDoRunner () {
         scanner = new Scanner(System.in);
-        todoList = new ArrayList<>();
+        todoList = new ToDoList();
     }
 
     public void startInterface() {
+        String jsonString = readFromFile(FILE_NAME);
         System.out.println("Welcome to T0-D0.");
-        printList(todoList);
-        for (int i = 0; i < 3; i++) {
-            System.out.println("Text of item to add?");
-            String text = scanner.nextLine();
-            todoList.add(new ToDoItem(text));
+        if (jsonString != null) {
+            todoList = ToDoList.jsonRestore(jsonString);
+            System.out.println(todoList);
         }
-        printList(todoList);
-        String jsonString = jsonSave(todoList);
-        System.out.println(jsonString);
-        List<ToDoItem> restoredList = jsonRestore(jsonString);
-        printList(restoredList);
-
+        /*for (int index = 0; index < 3; index++) {
+            System.out.println("Enter todo item:");
+            todoList.add(new ToDoItem(scanner.nextLine()));
+        }*/
+        mainMenu();
+        writeToFile(FILE_NAME, todoList.jsonSave());
     }
 
-    public void printList(List<ToDoItem> list) {
-        for (ToDoItem item : list) {
-            System.out.println(item);
-        }
-        if (list.size() == 0) {
-            System.out.println("(No items in list)");
+    public void mainMenu () {
+        System.out.println(todoList);
+        System.out.println("Options:");
+        System.out.println("1. Add a todo");
+        System.out.println("2. Remove or change a todo");
+        System.out.println("3. Clear list");
+        System.out.println("4. Exit");
+
+        int userChoice = Integer.parseInt(scanner.nextLine());
+        switch (userChoice) {
+            case 1:
+                System.out.println("Todo item to add?");
+                todoList.add(new ToDoItem(scanner.nextLine()));
+                mainMenu();
+                break;
+            case 2:
+                System.out.println("Index of item to remove/change?");
+                int userIndex = Integer.parseInt(scanner.nextLine());
+                userIndex--;
+                System.out.println("Remove, or change status? (r/c)");
+                String actionChoice = scanner.nextLine().toLowerCase();
+                if (actionChoice.contains("r")) {
+                    todoList.remove(userIndex);
+                } else if (actionChoice.contains("c")) {
+                    todoList.changeStatus(userIndex);
+                } else {
+                    System.out.println("You dun messed up good, kid.");
+                }
+                mainMenu();
+                break;
+            case 3:
+                //clear list
+                mainMenu();
+                break;
+            case 4:
+                //exit. Break/exit. Breaxit. Brexit! Gah
+                break;
         }
     }
 
-    public static String jsonSave(List<ToDoItem> todoList) {
-        JsonSerializer jsonSerializer = new JsonSerializer().deep(true);
-        String jsonString = jsonSerializer.serialize(todoList);
-        System.out.println("jsonString = " + jsonString);
-        return jsonString;
+    public void writeToFile (String fileName, String contents) {
+        try {
+            File file = new File(fileName);
+            FileWriter fileWriter = new FileWriter(file);
+            fileWriter.write(contents);
+            fileWriter.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 
-    public static List<ToDoItem> jsonRestore(String jsonTD) {
-        JsonParser toDoListParser = new JsonParser();
-        List<ToDoItem> item = toDoListParser.parse(jsonTD, List.class);
-
-        return item;
+    public String readFromFile (String fileName) {
+        try {
+            File file = new File(fileName);
+            Scanner fileScanner = new Scanner(file);
+            if (fileScanner.hasNext()) {
+                return fileScanner.nextLine();
+            }
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+        }
+        return null;
     }
 }
